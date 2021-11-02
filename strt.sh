e294d7ce8745ad728b7320157579a5d377df071b
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:49:03 +0700

    Update strt.sh

--- strt.sh
+++ strt.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
