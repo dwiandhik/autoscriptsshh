be89fac8facce480cff256df530232f00e7a7be2
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:46:04 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
 echo "Please input link to your vps data backup file."
 echo "You can check it on your email if you run backup data vps before."
