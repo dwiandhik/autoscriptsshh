0635b9a78a85081ec51f5ca9cb995e9c11615386
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:40:19 +0700

    Update renew-l2tp.sh

--- renew-l2tp.sh
+++ renew-l2tp.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
