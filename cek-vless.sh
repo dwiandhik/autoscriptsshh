07326d7dee0975ec9f1cf70ceaf376a4e39e5e38
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:30:13 +0700

    Update cek-vless.sh

--- cek-vless.sh
+++ cek-vless.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 echo -n > /tmp/other.txt
 data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-------------------------------";
