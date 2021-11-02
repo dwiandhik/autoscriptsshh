312767337de899f81a56112d24a13f6bcff2b8fd
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:48:37 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 clear
 IP=$(wget -qO- icanhazip.com);
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
