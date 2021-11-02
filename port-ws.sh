8268b1b6c2783f80160deeec33d23f242123d906
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:38:49 +0700

    Update port-ws.sh

--- port-ws.sh
+++ port-ws.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 clear
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
