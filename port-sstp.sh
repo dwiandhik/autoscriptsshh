813f070acd14ce0e544d53eb1338c9b086bb554a
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-02 22:39:11 +0700

    Update port-sstp.sh

--- port-sstp.sh
+++ port-sstp.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-exit 0
-fi
 clear
 sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
 echo -e "      Change Port $sstp"
