53f9b60ab4815f4ab03c54773066d3863e4d82e4
dwiandhik <46491631+dwiandhik@users.noreply.github.com>
2021-11-10 12:40:49 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/dwiandhik/autoscriptsshh/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://sc.vpnstores.net/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install
